028075d27373e9973db745fa5ac560f3d6cd044f
Goluchandan <99709975+Goluchandan@users.noreply.github.com>
2022-07-21 02:40:39 +0530

    day 1 all task done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import EmployeeTime from "./components/Products_Page/Employee_time/EmployeeTime";
+import SameSignUp from "./components/Products_Page/Same_Sign_up/Same_Sign-up";
+import SameSignUp2 from "./components/Products_Page/Same_Sign_up2/Same_Sign_up2";
 
 function App() {
   return (
-    <div className="App">
-      <h1>App Components come here</h1>
+    <div>
+  
+        <EmployeeTime />
+        <SameSignUp />
+        <SameSignUp2 />
+   
     </div>
   );
 }
