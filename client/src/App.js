fd5be71ce27a3b9a3544958b4b7e3e3bc7207377
abhinavBisht94 <98757679+abhinavBisht94@users.noreply.github.com>
2022-07-21 13:27:36 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
-import PricingPlan from "./components/PricingPage/MainPricingPage/PricingPlan"
+import PricingPlan from "./components/PricingPage/MainPricingPage/PricingPlan";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
 
 function App() {
   return (
     <div className="App">
       <PricingPlan />
+      {/* Pages Come here */}
+
+      <Signup />
+
+      {/* <Login /> */}
     </div>
   );
 }
