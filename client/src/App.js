058d5f5c2b17ce444359b6a5fcf1ae78e486e4ab
Chandan Kumar <99709975+Goluchandan@users.noreply.github.com>
2022-07-21 12:37:43 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,1 @@
-import "./App.css";
-import LargeWithLogoCentered from "./components/Homepage/Footer/Footer";
-import WithSubnavigation from "./components/Homepage/Navbar/Navbar";
-import Landing from "./components/Homepage/Mainhomepage/Landing";
 
-function App() {
-  return (
-    <div>
-      <WithSubnavigation />
-      <Landing />
-      <LargeWithLogoCentered />
-    </div>
-  );
-}
-
-export default App;
