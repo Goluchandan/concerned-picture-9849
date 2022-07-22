2ec0d3e792cc7a3be00fbaac3b0b4e799735c611
abhinavBisht94 <98757679+abhinavBisht94@users.noreply.github.com>
2022-07-22 22:21:10 +0530

    update

--- client/src/components/signup/SignupForm.jsx
+++ client/src/components/signup/SignupForm.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import "../../CSS/signup/signupForm.modules.css";
-import { useNavigate } from "react-router-dom";
 
 export const SignupForm = ({ googleButtonImage, appleButtonLogo }) => {
   const navigate = useNavigate();
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -38,6 +38,38 @@ authRouter.post("/signup", async (req, res) => {
   }
 });
 
+authRouter.post("/login", async (req, res) => {
+  // checking if the entered email is present in the database
+  let { email, password } = req.body;
+  if (!email) {
+    res.status(500).send({ message: "Error occurred" });
+  } else {
+    let searchResult = await UserSchema.find({
+      $and: [{ email: { $regex: email } }, { password: { $regex: password } }],
+    });
+    console.log("searchResult:", searchResult);
+    // console.log("searchResult:", searchResult[0].email);
+
+    if (searchResult.length === 0) {
+      res.status(500).send({ message: "Email or password is wrong" });
+    } else {
+      let check = false;
+
+      searchResult.map((e) => {
+        if (e.email === email && e.password === password) {
+          check = true;
+        }
+      });
+
+      console.log("check:", check);
+
+      if (!check) {
+        res.status(500).send({ message: "Entered Email or password is wrong" });
+      }
+    }
+  }
+});
+
 //---------------------------------------
 // Export
 module.exports = authRouter;
