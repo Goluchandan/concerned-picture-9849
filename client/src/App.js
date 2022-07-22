5e6d1ce4e5dc5a183dc6e78b1f756b3f6ace42a2
Goluchandan <99709975+Goluchandan@users.noreply.github.com>
2022-07-22 19:17:59 +0530

    day 4 all task done

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,12 @@
 import "./App.css";
+import React from "react"
+import BillingInvoicing from "./components/Products_Page/Billing&Invoicing/BillingInvoicing";
 
-import EmployeeTime from "./components/Products_Page/Employee_time/EmployeeTime";
-import PricingPlan from "./components/PricingPage/MainPricingPage/PricingPlan";
-import { Login } from "./pages/Login";
-import { Signup } from "./pages/Signup";
 
 function App() {
   return (
     <div >
-       <EmployeeTime />
-      {/* <PricingPlan /> */}
-      {/* Pages Come here */}
-      <Signup />
-      {/* <Login /> */}
+     <BillingInvoicing />
     </div>
   );
 }
