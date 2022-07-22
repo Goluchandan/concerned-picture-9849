c6032ff76cbad49dcae236716714a0f293b6a1d2
Goluchandan <99709975+Goluchandan@users.noreply.github.com>
2022-07-22 21:13:39 +0530

    day 4 all task done

--- client/src/components/Products_Page/Employee_time/Employee_FAQ/EmployeeFAQItem.jsx
+++ client/src/components/Products_Page/Employee_time/Employee_FAQ/EmployeeFAQItem.jsx
@@ -5,8 +5,8 @@ const EmployeeFAQItem = ({question, answer}) => {
 
     const [show , setShow] = useState(false)
   return (
-    <div className={styles.faq_question_h4_main_main_div}  >
-       <div className={styles.faq_question_h4_main_div}>
+    <div className={styles.faq_question_h4_main_main_div}  style= {show?{ backgroundColor:"rgb(229, 124, 216)"}:{backgroundColor:"#2C1338;", color:"rgb(252, 229, 216);" }}>
+       <div className={styles.faq_question_h4_main_div}  >
        
         {/* <h4 className={styles.faq_question_h4}>{question}</h4> */}
         <h4 className={styles.faq_question_h4} onClick={() =>setShow(!show)}>{question}</h4>
