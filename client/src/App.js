f3bf4ee72f81587e08b3a9e985f8d435ed039f3a
Goluchandan <99709975+Goluchandan@users.noreply.github.com>
2022-07-23 02:43:21 +0530

    day 5 all task done

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import React from "react"
 import BillingInvoicing from "./components/Products_Page/Billing&Invoicing/BillingInvoicing";
+import Payroll from "./components/Products_Page/Payroll/Payroll";
+import Reporting from "./components/Products_Page/Reporting&Transp/Reporting";
+import ProjectBudget from "./components/Products_Page/Project_Budget/ProjectsBudget";
 
 
 function App() {
   return (
     <div >
-     <BillingInvoicing />
+     {/* <BillingInvoicing /> */}
+     <Payroll />
+     <Reporting />
+     <ProjectBudget />
+
     </div>
   );
 }
